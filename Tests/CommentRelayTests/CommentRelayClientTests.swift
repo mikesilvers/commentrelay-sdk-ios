dd5ce8a7de8619f761e1242cf08876946737e7e1
Mike Silvers <d4h2i7i1kp38rv2mij16xumgwmnipa@bots.bitbucket.org>
2026-04-19 10:50:23 -0400

    Add CommentRelayClient.ping() hitting /health

---
+++ Tests/CommentRelayTests/CommentRelayClientTests.swift
@@ -0,0 +1,39 @@
+import XCTest
+@testable import CommentRelay
+
+final class CommentRelayClientTests: XCTestCase {
+    private var session: URLSession!
+
+    override func setUp() {
+        super.setUp()
+        MockURLProtocol.reset()
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [MockURLProtocol.self]
+        session = URLSession(configuration: config)
+    }
+
+    override func tearDown() {
+        MockURLProtocol.reset()
+        session = nil
+        super.tearDown()
+    }
+
+    func test_ping_issuesGetToHealthEndpoint() async throws {
+        MockURLProtocol.handler = { request in
+            let response = HTTPURLResponse(
+                url: request.url!, statusCode: 200,
+                httpVersion: "HTTP/1.1", headerFields: nil)!
+            return (response, Data())
+        }
+
+        let client = CommentRelayClient(
+            baseURL: URL(string: "http://localhost:3000")!,
+            session: session)
+        _ = try await client.ping()
+
+        XCTAssertEqual(MockURLProtocol.requests.count, 1)
+        let req = try XCTUnwrap(MockURLProtocol.requests.first)
+        XCTAssertEqual(req.url?.absoluteString, "http://localhost:3000/health")
+        XCTAssertEqual(req.httpMethod, "GET")
+    }
+}
