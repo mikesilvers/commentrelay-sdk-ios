30382d502ad72967a6d6a00a22bb91c183d5d3b6
Mike Silvers <d4h2i7i1kp38rv2mij16xumgwmnipa@bots.bitbucket.org>
2026-04-19 11:09:32 -0400

    Test CommentRelayClient.ping() returns true on 200

--- Tests/CommentRelayTests/CommentRelayClientTests.swift
+++ Tests/CommentRelayTests/CommentRelayClientTests.swift
@@ -36,4 +36,20 @@ final class CommentRelayClientTests: XCTestCase {
         XCTAssertEqual(req.url?.absoluteString, "http://localhost:3000/health")
         XCTAssertEqual(req.httpMethod, "GET")
     }
+
+    func test_ping_returnsTrue_on200() async throws {
+        MockURLProtocol.handler = { request in
+            let response = HTTPURLResponse(
+                url: request.url!, statusCode: 200,
+                httpVersion: "HTTP/1.1", headerFields: nil)!
+            return (response, Data())
+        }
+
+        let client = CommentRelayClient(
+            baseURL: URL(string: "http://localhost:3000")!,
+            session: session)
+        let ok = try await client.ping()
+
+        XCTAssertTrue(ok)
+    }
 }
